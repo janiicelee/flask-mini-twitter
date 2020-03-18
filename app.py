8b6a16d15363a82cf4585673b0634f0418b497a0
Yerin Lee <meekukin@gmail.com>
2020-03-18 21:04:37 +0900

    add gitignore

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 from flask      import Flask, jsonify, request
 from flask.json import JSONEncoder
+from sqlalchemy import create_engine, text
+
+def create_app(test_config = None):
+    app = Flask(__name__)
+
+    if test_config is None:
+        app.config.from_pyfile("config.py")
+    else:
+        app.config.update(test_config)
+
+    database = create_engine(app.config['DB_URL'], encoding='utf-8', max_overflow = 0)
+    app.database = database
+
+    return app 
 
 app          = Flask(__name__)
 app.id_count = 1
 app.users    = {}
-
-
-@app.route("/ping", methods=['GET'])
-def ping():
-    return "pong"
 
 @app.route("/sign-up", methods=['POST'])
 def sign_up():
